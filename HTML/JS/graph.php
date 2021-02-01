7987c33deb5d9e6a008a5cf40c9b969f40891b2b
goddarkhun <zsolt211@yahoo.com>
2021-02-01 17:50:34 +0200

    Added more graphs

--- HTML/JS/graph.php
+++ HTML/JS/graph.php
@@ -1,11 +1,106 @@
 <?php
 
-$Pressure=0;
-$Temperature=0;
-$Humidity=0;
-$t_x=0;
+class Graph3D
+{
+    public $div;
+    public $mode;
+    public $name;
+    public $name0;
+    public $name1;
+    public $name2;
+    public $t0;
+    public $t1;
+    public $t2;
+    public $color;
+    public $trace;
+    public $data;
+    public $layout;
+    public $config;
 
+    function __construct($div,$mode,$name,$name0,$name1,$name2,$color)
+    {
+        $this->div = $div;
+        $this->mode = $mode;
+        $this->name0=$name0;
+        $this->name1= $name1;
+        $this->name2= $name2;
+        $this->name=$name;
+        $this->color = $color;
 
+        $con=mysqli_connect("exo.xdd.ro","user","pass","exo") or die("adatbazis_hiba");
+        $queryString='select '.$this->name0.','.$this->name1.','.$this->name2.' from sensor_data limit 10;';
+        $res=mysqli_query($con,$queryString);
+        if($res)
+        {
+            $sor=mysqli_fetch_assoc($res);
+            $this->t0=$sor[$this->name0];
+            $this->t1=$sor[$this->name1];
+            $this->t2=$sor[$this->name2];
+
+        }
+        $this->trace='{
+            type: "scatter3d",
+            mode:"'.$this->mode.'",
+            name:"'.$this->name.'",
+            visible: true,
+            x:['.$this->t0.'],
+            y:['.$this->t1.'],
+            z:['.$this->t2.'],
+
+            line: {
+              width: 2,
+              color:"'.$this->color.'",
+              },
+            marker: {
+              size: 3.5,
+              color:"'.$this->color.'",
+
+              }
+
+            }';
+        $this->data='['.$this->trace.']';
+
+        $this->layout ='{
+            autosize: false,
+            width: 500,
+            height: 500,
+            margin: {
+              l: 0,
+              r: 0,
+              b: 50,
+              t: 100
+            }
+          }';
+          $this->config='{
+            responsive: true,
+            displaylogo:false
+          }';
+        echo '<script>
+        Plotly.newPlot("'.$div.'",'.$this->data.','.$this->layout.','.$this->config.');
+        </script> ';
+
+        if($res)
+        {
+            while($sor=mysqli_fetch_assoc($res))
+            {
+
+                $this->t0=$sor[$this->name0];
+                $this->t1=$sor[$this->name1];
+                $this->t2=$sor[$this->name2];
+
+            $data_update = '{
+                x:[['.$this->t0.']],
+                y:[['.$this->t1.']],
+                z:[['.$this->t2.']]
+            }';
+            echo'
+            <script>
+            Plotly.extendTraces('.$this->div.','.$data_update.',[0])
+            </script>';
+            }
+        }
+    }
+}
 
 class Graph2D
 {
@@ -29,136 +124,258 @@ class Graph2D
     public $config;
 
     function __construct($div,$mode,$name0,$name1,$name2,$color0,$color1,$color2) {
-        $this->div = $div;
-        $this->mode = $mode;
-        $this->name0 = $name0;
-        $this->name1 = $name1;
-        $this->name2 = $name2;
-        $this->t0 = 0;
-        $this->t1 = 0;
-        $this->t2 = 0;
-        $this->t_x = 0;
-        $this->color0 = $color0;
-        $this->color1 = $color1;
-        $this->color2 = $color2;
+        if($name2=="")
+        {
+            $this->div = $div;
+            $this->mode = $mode;
+            $this->name0 = $name0;
+            $this->name1 = $name1;
+            $this->t0 = 0;
+            $this->t1 = 0;
+            $this->t_x = 0;
+            $this->color0 = $color0;
+            $this->color1 = $color1;
 
-        $con=mysqli_connect("HOST","USER","PASS","DB") or die("adatbazis_hiba");
-        $queryString='select '.$this->name0.','.$this->name1.','.$this->name2.' from sensor_data limit 10;';
-        echo $queryString;
-        $res=mysqli_query($con,$queryString);
+            $con=mysqli_connect("exo.xdd.ro","user","pass","exo") or die("adatbazis_hiba");
+            $queryString='select '.$this->name0.','.$this->name1.' from sensor_data limit 10;';
+            $res=mysqli_query($con,$queryString);
 
-        if($res)
-			{
-				$sor=mysqli_fetch_assoc($res);
+            if($res)
+            {
+                $sor=mysqli_fetch_assoc($res);
+                $this->t0=$sor[$this->name0];
+                $this->t1=$sor[$this->name1];
+            }
+
+            $this->trace0='{
+                type: "scatter",
+                mode:"'.$this->mode.'",
+                name:"'.$this->name0.'",
+                x:['.$this->t_x.'],
+                y:['.$this->t0.'],
+                line: {
+                width: 1,
+                color:"'.$this->color0.'",
+                },
+                marker: {
+                size: 3.5,
+                color:"'.$this->color0.'",
+
+                }
+
+                }';
+
+            $this->trace1='{
+                type: "scatter",
+                mode:"'.$this->mode.'",
+                name:"'.$this->name1.'",
+                x:['.$this->t_x.'],
+                y:['.$this->t1.'],
+                line: {
+                    width: 1,
+                    color:"'.$this->color1.'",
+                    },
+                marker: {
+                    size: 3.5,
+                    color:"'.$this->color1.'",
+
+                    }
+
+                }';
+            $this->layout = '{
+                autosize: false,
+                width: 500,
+                height: 500,
+                margin: {
+                l: 50,
+                r: 0,
+                b: 50,
+                t: 150
+                },
+                yaxis: {
+                range: [0,100]
+                }
+            }';
+            $this->config='{
+                responsive: true,
+                displaylogo:false
+            }';
+            $this->data='['.$this->trace0.','.$this->trace1.']';
+            echo '<script>
+            Plotly.newPlot("'.$div.'",'.$this->data.','.$this->layout.','.$this->config.');
+            </script>';
+
+            if($res)
+            {
+                    while($sor=mysqli_fetch_assoc($res))
+                {
+
+                        $this->t_x++;
+                        $this->t0=$sor[$this->name0];
+                        $this->t1=$sor[$this->name1];
+
+
+                        $data_update = '{
+                            y: [['.$this->t0.'],['.$this->t1.']],
+                            x: [['.$this->t_x.'],['.$this->t_x.']]
+                        }';
+                        echo' <script>
+                        Plotly.extendTraces('.$this->div.','.$data_update.',[0,1])
+                        </script>';
+                }
+
+            }
+        }
+        else
+        {
+            $this->div = $div;
+            $this->mode = $mode;
+            $this->name0 = $name0;
+            $this->name1 = $name1;
+            $this->name2 = $name2;
+            $this->t0 = 0;
+            $this->t1 = 0;
+            $this->t2 = 0;
+            $this->t_x = 0;
+            $this->color0 = $color0;
+            $this->color1 = $color1;
+            $this->color2 = $color2;
+
+            $con=mysqli_connect("exo.xdd.ro","user","pass","exo") or die("adatbazis_hiba");
+            $queryString='select '.$this->name0.','.$this->name1.','.$this->name2.' from sensor_data limit 10;';
+            $res=mysqli_query($con,$queryString);
+
+            if($res)
+            {
+                $sor=mysqli_fetch_assoc($res);
                 $this->t0=$sor[$this->name0];
                 $this->t1=$sor[$this->name1];
                 $this->t2=$sor[$this->name2];
             }
 
-        $this->trace0='{
-            type: "scatter",
-            mode:"'.$this->mode.'",
-            name:"'.$this->name0.'",
-            x:['.$this->t_x.'],
-            y:['.$this->t0.'],
-            line: {
-              width: 1,
-              color:"'.$this->color0.'",
-              },
-            marker: {
-              size: 3.5,
-              color:"'.$this->color0.'",
 
-              }
-
-            }';
-
-        $this->trace1='{
-            type: "scatter",
-            mode:"'.$this->mode.'",
-            name:"'.$this->name1.'",
-            x:['.$this->t_x.'],
-            y:['.$this->t1.'],
-            line: {
+            $this->trace0='{
+                type: "scatter",
+                mode:"'.$this->mode.'",
+                name:"'.$this->name0.'",
+                x:['.$this->t_x.'],
+                y:['.$this->t0.'],
+                line: {
                 width: 1,
-                color:"'.$this->color1.'",
+                color:"'.$this->color0.'",
                 },
-            marker: {
+                marker: {
                 size: 3.5,
-                color:"'.$this->color1.'",
+                color:"'.$this->color0.'",
 
                 }
 
-            }';
-        $this->trace2='{
-            type: "scatter",
-            mode:"'.$this->mode.'",
-            name:"'.$this->name2.'",
-            x:['.$this->t_x.'],
-            y:['.$this->t2.'],
-            line: {
-                width: 1,
-                color:"'.$this->color2.'",
-                },
-            marker: {
-                size: 3.5,
-                color:"'.$this->color2.'",
+                }';
 
+            $this->trace1='{
+                type: "scatter",
+                mode:"'.$this->mode.'",
+                name:"'.$this->name1.'",
+                x:['.$this->t_x.'],
+                y:['.$this->t1.'],
+                line: {
+                    width: 1,
+                    color:"'.$this->color1.'",
+                    },
+                marker: {
+                    size: 3.5,
+                    color:"'.$this->color1.'",
+
+                    }
+
+                }';
+            $this->trace2='{
+                type: "scatter",
+                mode:"'.$this->mode.'",
+                name:"'.$this->name2.'",
+                x:['.$this->t_x.'],
+                y:['.$this->t2.'],
+                line: {
+                    width: 1,
+                    color:"'.$this->color2.'",
+                    },
+                marker: {
+                    size: 3.5,
+                    color:"'.$this->color2.'",
+
+                    }
+
+                }';
+            $this->layout = '{
+                autosize: false,
+                width: 500,
+                height: 500,
+                margin: {
+                l: 50,
+                r: 0,
+                b: 50,
+                t: 150
+                },
+                yaxis: {
+                range: [0,100]
+                }
+            }';
+            $this->config='{
+                responsive: true,
+                displaylogo:false
+            }';
+            $this->data='['.$this->trace0.','.$this->trace1.','.$this->trace2.']';
+            echo '<script>
+            Plotly.newPlot("'.$div.'",'.$this->data.','.$this->layout.','.$this->config.');
+            </script>';
+
+            if($res)
+            {
+                    while($sor=mysqli_fetch_assoc($res))
+                {
+
+                        $this->t_x++;
+                        $this->t0=$sor[$this->name0];
+                        $this->t1=$sor[$this->name1];
+                        $this->t2=$sor[$this->name2];
+
+
+                        $data_update = '{
+                            y: [['.$this->t0.'],['.$this->t1.'],['.$this->t2.']],
+                            x: [['.$this->t_x.'],['.$this->t_x.'],['.$this->t_x.']]
+                        }';
+                        echo' <script>
+                        Plotly.extendTraces('.$this->div.','.$data_update.',[0,1,2])
+                        </script>';
                 }
 
-            }';
-        $this->layout = '{
-            autosize: false,
-            width: 500,
-            height: 500,
-            margin: {
-            l: 50,
-            r: 0,
-            b: 50,
-            t: 150
-            },
-            yaxis: {
-            range: [0,100]
             }
-        }';
-        $this->config='{
-            responsive: true,
-			displaylogo:false
-        }';
-        $this->data='['.$this->trace0.','.$this->trace1.','.$this->trace2.']';
-        echo '<script>
-        Plotly.newPlot("'.$div.'",'.$this->data.','.$this->layout.','.$this->config.');
-        </script>';
-
-        if($res)
-			{
-				while($sor=mysqli_fetch_assoc($res))
-			   {
-
-                    $this->t_x++;
-                    $this->t0=$sor[$this->name0];
-                    $this->t1=$sor[$this->name1];
-                    $this->t2=$sor[$this->name2];
-
-
-                    $data_update = '{
-                        y: [['.$this->t0.'],['.$this->t1.'],['.$this->t2.']],
-                        x: [['.$this->t_x.'],['.$this->t_x.'],['.$this->t_x.']]
-                    }';
-                    echo' <script>
-                    Plotly.extendTraces('.$this->div.','.$data_update.',[0,1,2])
-                    </script>';
-			   }
-
-			}
-      }
-    function Update()
-    {
-
+        }
     }
+      function Line()
+      {
+          if($this->mode=='lines+markers')
+          {
+            $this->mode='markers';
+          }
+          else if($this->mode=='markers')
+          {
+            $this->mode='lines+markers';
+          }
+          $update='{
+              mode:"'.$this->mode.'"
+          }';
+          echo'<script>
+          Plotly.restyle("'.$this->div.'",'.$update.');
+          </script>';
+      }
 }
-$GraphAir = new Graph2D('Div2D','lines+markers','presssure','temperature','humidity','red','green','blue');
-$GraphLight = new Graph2D('MyDiv','lines+markers','uv_index','ir_light','visible_light','red','green','blue');
+
+
+$GraphGyro = new Graph3D('Div1','lines+markers','Gyro','gyro_x','gyro_y','gyro_z',"red");
+$GraphAir = new Graph2D('Div2','lines+markers','presssure','temperature','humidity','red','green','blue');
+$GraphLight = new Graph2D('Div3','lines+markers','uv_index','ir_light','visible_light','red','green','blue');
+$GraphGas = new Graph2D('Div4','lines+markers','eco2','tvoc','','red','green','');
 
 
 ?>
--- HTML/index.php
+++ HTML/index.php
@@ -1,26 +1,54 @@
-<?php
-echo "
+
 <html>
 <head>
-	 <link rel='stylesheet' href='../CSS/graph.css'>
+	 <link rel='stylesheet' href='CSS/graph.css'>
+	 <script>
+		function Line(str)
+		{
+			if(str=="Line On")
+			{
+				var update = {
+					mode:"markers"
+				};
+				Plotly.restyle("Div1", update);
+				Plotly.restyle("Div2", update);
+				Plotly.restyle("Div3", update);
+				Plotly.restyle("Div4", update);
+
+				document.getElementById("vonal").value="Line Off"
+			}
+			else if(str=="Line Off")
+			{
+				var update = {
+					mode:"lines+markers"
+				};
+				Plotly.restyle("Div1", update);
+				Plotly.restyle("Div2", update);
+				Plotly.restyle("Div3", update);
+				Plotly.restyle("Div4", update);
+
+				document.getElementById("vonal").value="Line On"
+			}
+		}
+	</script>
 </head>
 
 <body>
 
 
-	<div class = 'intro' id='Div2D' ></div>
-	<div class = 'intro' id='Div3D' ></div>
-	<div class = 'intro' id='MyDiv' ></div>
+	<div class = 'intro' id='Div1' ></div>
+	<div class = 'intro' id='Div2' ></div>
+	<div class = 'intro' id='Div3' ></div>
+	<div class = 'intro' id='Div4' ></div>
 
 
-	<input type=button onclick='Line()' id='vonal' value='Line On' />
+	<input type='button' name='vonal' id='vonal' value='Line On' onclick='Line(this.value)' />
 
-
-	<script src='JS/plotly-latest.min.js'></script> ";
+	<script src='JS/plotly-latest.min.js'></script>
+	<?php
 	include_once('JS/graph.php');
+	?>
 
-	"
+
 </body>
 </html>
-"
-?>
